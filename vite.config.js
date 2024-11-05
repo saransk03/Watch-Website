57011c41cb5946caf543c0f063cc3d8587d5adc8
saransk03 <saransk753@gmail.com>
2024-11-05 12:22:07 +0530

    update

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,16 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  plugins: [react()],
+  base: './',  // Ensures that paths are relative in production
+  resolve: {
+    alias: {
+      '@': '/src',
+    },
+  },
+  build: {
+    rollupOptions: {
+      input: './src/main.jsx',
+    },
+  },
 })
